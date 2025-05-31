1b5494c4106da17708bc9ebeee1e6e508a69f1b7
Gaurav Rayat <gaurav.rayat2004@gmail.com>
2025-05-31 11:13:27 +0530

    Certificates

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from main import app
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)),debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
